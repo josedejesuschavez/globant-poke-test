8530296cef26568189d6f3b6247bda1758a35a55
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2023-06-06 04:08:54 -0500

    Completed feature for get all berries and get berry by id

--- backend/berry/infraestructure/pokeapi_repository.py
+++ backend/berry/infraestructure/pokeapi_repository.py
@@ -1,16 +1,18 @@
+import os
+
 import requests
 from backend.shared.domain.repository import Repository
 from backend.shared.infraestructure.cache import cache
 
 
 class PokeAPIRepository(Repository):
-
+    api = os.environ['api']
     def __init__(self):
         pass
 
     @cache.memoize(timeout=120)
     def get_all(self):
-        url = "https://pokeapi.co/api/v2/berry/?offset=0&limit=1000"
+        url = f"{PokeAPIRepository.api}/?offset=0&limit=1000"
         response = requests.get(url)
 
         if response.status_code == 200:
@@ -19,7 +21,7 @@ class PokeAPIRepository(Repository):
 
     @cache.memoize(timeout=120)
     def get_by_id(self, id: int):
-        url = f"https://pokeapi.co/api/v2/berry/{id}"
+        url = f"{PokeAPIRepository.api}/{id}"
         response = requests.get(url)
 
         if response.status_code == 200:
